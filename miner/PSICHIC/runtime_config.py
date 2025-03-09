f9948d84f35d8bd4b6c80be8a26a5adbdff26409
EdiMarian <ichimedimarian@gmail.com>
2025-03-09 23:12:21 +0200

    massive improvement

--- miner/PSICHIC/runtime_config.py
+++ miner/PSICHIC/runtime_config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from dotenv import load_dotenv
+import torch
 
 load_dotenv()
 
@@ -9,5 +10,11 @@ class RuntimeConfig:
     device = os.environ.get("DEVICE_OVERRIDE")
     DEVICE = "cuda:0"  # Forțat pe primul GPU
     MODEL_PATH = os.path.join(PSICHIC_PATH, 'trained_weights', 'PDBv2020_PSICHIC')
-    BATCH_SIZE = 4096
+    BATCH_SIZE = 16384  # Increased from 4096
+    MODEL_CACHE = True  # Added for memory caching
     
+    # Enable CUDA optimizations
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
--- miner/PSICHIC/wrapper.py
+++ miner/PSICHIC/wrapper.py
@@ -67,21 +67,21 @@ class PsichicWrapper:
     
     def create_screen_loader(self, protein_dict, smiles_dict):
         self.screen_df = pd.DataFrame({'Protein': [k for k in self.protein_seq for _ in self.smiles_list],
-                                       'Ligand': [l for l in self.smiles_list for _ in self.protein_seq],
-                                       })
+                                    'Ligand': [l for l in self.smiles_list for _ in self.protein_seq],
+                                    })
         
         dataset = ProteinMoleculeDataset(self.screen_df, 
-                                         smiles_dict, 
-                                         protein_dict, 
-                                         device=self.device
-                                         )
+                                        smiles_dict, 
+                                        protein_dict, 
+                                        device=self.device
+                                        )
         
         self.screen_loader = DataLoader(dataset,
                                         batch_size=self.runtime_config.BATCH_SIZE,
                                         shuffle=False,
                                         follow_batch=['mol_x', 'clique_x', 'prot_node_aa'],
-                                        num_workers=32,         # Mai mulți workers
-                                        prefetch_factor=16,      # Pre-încărcare extinsă
+                                        num_workers=64,         # Increased from 32
+                                        prefetch_factor=32,      # Increased from 16
                                         pin_memory=True,
                                         persistent_workers=True
                                         )
@@ -95,14 +95,69 @@ class PsichicWrapper:
         self.smiles_dict = self.initialize_smiles(smiles_list)
         torch.cuda.empty_cache()
         self.create_screen_loader(self.protein_dict, self.smiles_dict)
-        self.screen_df = virtual_screening(self.screen_df, 
-                                           self.model, 
-                                           self.screen_loader,
-                                           os.getcwd(),
-                                           save_interpret=False,
-                                           ligand_dict=self.smiles_dict, 
-                                           device=self.device,
-                                           save_cluster=False,
-                                           )
-        return self.screen_df
+        
+        # Create FP16 scaler for mixed precision
+        scaler = torch.cuda.amp.GradScaler()
+        
+        # Run with mixed precision
+        with torch.no_grad():
+            reg_preds = []
+            cls_preds = []
+            mcls_preds = []
+            interaction_keys_all = []
+            attention_dicts = []
+            
+            self.model.eval()
+            
+            for data in tqdm(self.screen_loader):
+                data = data.to(self.device)
+                
+                # Use mixed precision for faster computation
+                with torch.cuda.amp.autocast():
+                    reg_pred, cls_pred, mcls_pred, sp_loss, o_loss, cl_loss, attention_dict = self.model(
+                            # Molecule
+                            mol_x=data.mol_x, mol_x_feat=data.mol_x_feat, bond_x=data.mol_edge_attr,
+                            atom_edge_index=data.mol_edge_index, clique_x=data.clique_x, 
+                            clique_edge_index=data.clique_edge_index, atom2clique_index=data.atom2clique_index,
+                            # Protein
+                            residue_x=data.prot_node_aa, residue_evo_x=data.prot_node_evo,
+                            residue_edge_index=data.prot_edge_index,
+                            residue_edge_weight=data.prot_edge_weight,
+                            # Mol-Protein Interaction batch
+                            mol_batch=data.mol_x_batch, prot_batch=data.prot_node_aa_batch, clique_batch=data.clique_x_batch,
+                            # save_cluster
+                            save_cluster=False
+                    )
+                
+                interaction_keys = list(zip(data.prot_key, data.mol_key))
+                interaction_keys_all.extend(interaction_keys)
+                
+                if reg_pred is not None:
+                    reg_pred = reg_pred.squeeze().reshape(-1).cpu().numpy()
+                    reg_preds.append(reg_pred)
+                    
+                if cls_pred is not None:
+                    cls_pred = torch.sigmoid(cls_pred).squeeze().reshape(-1).cpu().numpy()
+                    cls_preds.append(cls_pred)
+
+                if mcls_pred is not None:
+                    mcls_pred = torch.softmax(mcls_pred,dim=-1).cpu().numpy()
+                    mcls_preds.append(mcls_pred)
+                    
+                attention_dicts.append(attention_dict)
+                
+            # Combine results
+            df_results = pd.DataFrame(interaction_keys_all, columns=['Protein', 'Ligand'])
+            if reg_preds:
+                all_reg_preds = np.concatenate(reg_preds)
+                df_results['predicted_binding_affinity'] = all_reg_preds
+            
+            if cls_preds:
+                all_cls_preds = np.concatenate(cls_preds)
+                df_results['predicted_binary_interaction'] = all_cls_preds
+                
+            # Free memory
+            torch.cuda.empty_cache()
+            
+        return df_results
         
